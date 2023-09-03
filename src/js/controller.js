ee9099018b1b34b7aea7ea3c7fdb59e7c7eccdde
davidmaxy1994 <maksymiukdavid@gmail.com>
2023-09-03 10:12:08 +0100

    Added new welcome feature

--- src/js/controller.js
+++ src/js/controller.js
@@ -158,6 +158,10 @@ const controlAddRecipe = async function (newRecipe) {
   }
 };
 
+const newFeature = function () {
+  console.log('Welcome to the new branch! :)');
+};
+
 const init = function () {
   bookmarksView.addHandlerRender(controlBookmarks);
   recipeView.addHandlerRender(controlRecipes);
@@ -166,6 +170,7 @@ const init = function () {
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerClick(controlPagination);
   addRecipeView.addHandlerUpload(controlAddRecipe);
+  newFeature();
 };
 init();
 
