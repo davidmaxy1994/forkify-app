7dba78039cdb37f5c645ebcd3eea25c41dfa927f
davidmaxy1994 <maksymiukdavid@gmail.com>
2023-09-04 10:17:50 +0100

    Cleaned up controller.js

--- src/js/controller.js
+++ src/js/controller.js
@@ -158,10 +158,6 @@ const controlAddRecipe = async function (newRecipe) {
   }
 };
 
-const newFeature = function () {
-  console.log('Welcome to the new branch! :)');
-};
-
 const init = function () {
   bookmarksView.addHandlerRender(controlBookmarks);
   recipeView.addHandlerRender(controlRecipes);
@@ -170,7 +166,6 @@ const init = function () {
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerClick(controlPagination);
   addRecipeView.addHandlerUpload(controlAddRecipe);
-  newFeature();
 };
 init();
 
